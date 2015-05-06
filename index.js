6fa96674ecc8f38d30e4738688024dddb22d45ae
Yunzheng <42thcoder@gmail.com>
2015-05-06 13:36:09 +0800

    once

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
-var _ = {
-  // ...
+var _ = {};
+
+_.once = function(fn){
+  var invoked = false,
+      result;
+  
+  return function(){
+    result = invoked ? result : fn();
+    invoked = true;
+    
+    return result;
+  };
 };
+
 
 module.exports = _;
